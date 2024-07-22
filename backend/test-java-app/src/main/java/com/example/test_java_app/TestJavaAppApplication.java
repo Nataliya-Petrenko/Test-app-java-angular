16912b725f286dc9caa5b34618afc9e8d10b9d30
Nataliya <nataliya_petrenko_@ukr.net>
2024-07-22 15:46:59 +0300

    start Spring app

---
+++ backend/test-java-app/src/main/java/com/example/test_java_app/TestJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_java_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaAppApplication.class, args);
+	}
+
+}
---
+++ backend/test-java-app/src/test/java/com/example/test_java_app/TestJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
